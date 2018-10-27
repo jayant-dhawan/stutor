507a7abc7f7be7dfb4a7c7981423f52f9023d4b5
Jayant <jayantdhawan95@gmail.com>
2018-10-28 04:11:58 +0530

    SignIn Implementation

--- app/src/main/java/com/stutor/android/MainActivity.java
+++ app/src/main/java/com/stutor/android/MainActivity.java
@@ -1,13 +1,40 @@
 package com.stutor.android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.firebase.auth.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //initializing firebase auth object
+                mAuth = FirebaseAuth.getInstance();
+
+        //if getCurrentUser does not returns null
+        if(mAuth.getCurrentUser() != null){
+            //that means user is already logged in
+            //and open profile activity
+            Toast.makeText(getApplicationContext(), "User Logged In", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Not Logged In", Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+        }
     }
 }
