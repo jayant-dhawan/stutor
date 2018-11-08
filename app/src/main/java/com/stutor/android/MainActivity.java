76c130bfc553c568d42c7fb695819f9c9e74b7f5
Jayant <jayantdhawan95@gmail.com>
2018-11-09 03:25:44 +0530

    SignIn Implementation

--- app/src/main/java/com/stutor/android/MainActivity.java
+++ app/src/main/java/com/stutor/android/MainActivity.java
@@ -2,10 +2,11 @@ package com.stutor.android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.google.firebase.auth.*;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,24 +18,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-    }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        //initializing firebase auth object
+        int SPLASH_TIME_OUT = 3000;
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
                 mAuth = FirebaseAuth.getInstance();
 
-        //if getCurrentUser does not returns null
-        if(mAuth.getCurrentUser() != null){
-            //that means user is already logged in
-            //and open profile activity
-            Toast.makeText(getApplicationContext(), "User Logged In", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "Not Logged In", Toast.LENGTH_SHORT).show();
-            finish();
-            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
-        }
+                // Check if user is signed in (non-null) and update UI accordingly.
+                //initializing firebase auth object
+                //if getCurrentUser does not returns null
+                if (mAuth.getCurrentUser() != null) {
+                    //that means user is already logged in
+                    //and open profile activity
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                } else {
+                    Toast.makeText(MainActivity.this, "Not Logged In", Toast.LENGTH_SHORT).show();
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                }
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
+
 }
--- app/src/main/java/com/stutor/android/SignInActivity.java
+++ app/src/main/java/com/stutor/android/SignInActivity.java
@@ -1,13 +1,102 @@
 package com.stutor.android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private Button signinButton;
+    private ProgressBar progressBar;
+    private TextView forgotPassword;
+    private TextView SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        inputEmail = findViewById(R.id.emailEditText);
+        inputPassword = findViewById(R.id.passwordEditText);
+        signinButton = findViewById(R.id.SignInButton);
+        progressBar = findViewById(R.id.progressBar);
+        forgotPassword = findViewById(R.id.forgotPassword);
+        SignUp = findViewById(R.id.SignUpLink);
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this, ResetPasswordActivity.class));
+            }
+        });
+
+        auth = FirebaseAuth.getInstance();
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        inputPassword.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(SignInActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(SignInActivity.this, DashboardActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 }
