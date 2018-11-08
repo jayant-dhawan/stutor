81984f2d85c454b7a90af0aa4d63d56b80832ef9
Jayant <jayantdhawan95@gmail.com>
2018-11-09 03:27:21 +0530

    SignIn Implementation

---
+++ app/src/main/java/com/stutor/android/DashboardActivity.java
@@ -0,0 +1,13 @@
+package com.stutor.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
